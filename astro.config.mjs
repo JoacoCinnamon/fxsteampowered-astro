5ba07e2136abfb9746c452034d506d67c29743a5
JoacoCinnamon <joacopontello2002@gmail.com>
2024-09-20 12:57:24 -0300

    Replace cloudflare with netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare()
+	output: "server",
+	adapter: netlify(),
 });
