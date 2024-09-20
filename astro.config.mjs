4801591eb4a1fd40ededc13cf48b35b743e8ab5c
JoacoCinnamon <joacopontello2002@gmail.com>
2024-09-20 04:25:58 -0300

    wip: Add node polyfills to compile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare()
+  output: "server",
+  adapter: cloudflare(),
+  vite: {
+    plugins: [nodePolyfills()],
+  },
 });
